708fcc3273d89294fe8eabefd1ee4d9b33e74fda
danielsidauruk <danielsidauruk@users.noreply.github.com>
2025-08-06 01:09:16 +0700

    feat(backend): add autoscalling group and load balancer

---
+++ src/aws/modules/backend/iam.tf
@@ -0,0 +1,45 @@
+
+resource "aws_iam_role" "ec2_secrets_manager_role" {
+  name = "${var.application_name}-ec2-secrets-manager-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "secrets_manager_policy" {
+  name        = "${var.application_name}-secrets-manager-policy"
+  description = "Policy to allow reading secrets from AWS Secrets Manager"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowSecretsManagerRead"
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Resource = [var.mongodb_secret_arn]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "secrets_manager_attachment" {
+  role       = aws_iam_role.ec2_secrets_manager_role.name
+  policy_arn = aws_iam_policy.secrets_manager_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.application_name}-ec2-profile"
+  role = aws_iam_role.ec2_secrets_manager_role.name
+}
